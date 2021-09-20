30c79ee3db191a250d8af9dbdc5b6e374d3d9f54
mhidjriansyah <mhidjriansyah20@gmail.com>
2021-09-20 12:36:37 +0700

    menambahkan TugasP1.php

---
+++ Contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh_1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhamad Hidjriansyah<br>
+        Saya mahasiswa UBSI Jurusan Sistem Informasi";
+    }
+}
+
+
+
---
+++ TugasP1.php
@@ -0,0 +1,10 @@
+<?php
+class TugasP1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo "Nama saya Muhamad Hidjriansyah<br> Nik 19200570<br> Kelas 19.3D.25<br> Saya tinggal
+di cengkareng jakarta barat";
+}
+}
